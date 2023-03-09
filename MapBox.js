eaa6401e908963911e8e850cccf083e3e02ebd27
SalaMulti <atedi.uaq@gmail.com>
2023-03-09 14:23:29 -0600

    first commit

---
+++ MapBox.js
@@ -0,0 +1,7 @@
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3N3dWFxMTIzIiwiYSI6ImNsZXhnd29wdTAzbzkzcm8ydmMweHN3bG0ifQ.7cndXESi9y9bSwluO-5msQ'
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/oswuaq123/clexh5zif000w01mg6diszskj'
+  });
